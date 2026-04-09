cb696ed1efe38c660ad3394bf37b70f6af79f4ed
dwolav200 <David_Wolaver@cable.comcast.com>
2026-04-09 15:47:22 -0400

    more stuff added

--- src/xr-audio/xraudio_mfv.h
+++ src/xr-audio/xraudio_mfv.h
@@ -23,6 +23,33 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <jansson.h>
+
+#ifndef XRAUDIO_MFV_API_VERSION
+#define XRAUDIO_MFV_API_VERSION 1
+#endif
+
+// Capabilities
+#define XRAUDIO_MFV_CAPS_NONE               (0x0000)
+#define XRAUDIO_MFV_CAPS_AUDIO_GAIN         (0x0001) // Supports audio gain adjustment on the input audio stream
+#define XRAUDIO_MFV_CAPS_KWD_VALIDATION     (0x0002) // Supports keyword validation
+#define XRAUDIO_MFV_CAPS_EOS_DETECTION      (0x0004) // Supports end-of-speech detection
+#define XRAUDIO_MFV_CAPS_REFERENCE_AUDIO    (0x0008) // Supports reference audio stream (e.g. keyword detection in reference audio)
+
+typedef enum {
+   XRAUDIO_MFV_RESULT_SUCCESS        = 0,
+   XRAUDIO_MFV_RESULT_ERROR_PARAMS   = 1,
+   XRAUDIO_MFV_RESULT_ERROR_INTERNAL = 2,
+   XRAUDIO_MFV_RESULT_INVALID        = 3,
+} xraudio_mfv_result_t;
+
+typedef enum {
+   XRAUDIO_MFV_EOS_RESULT_UNAVAILABLE     = 0,
+   XRAUDIO_MFV_EOS_RESULT_SUCCESS         = 1,
+   XRAUDIO_MFV_EOS_RESULT_TIMEOUT_INITIAL = 2,
+   XRAUDIO_MFV_EOS_RESULT_TIMEOUT_FINAL   = 3,
+   XRAUDIO_MFV_EOS_RESULT_INVALID         = 4,
+} xraudio_mfv_eos_result_t;
 
 typedef enum {
    XRAUDIO_MFV_MSG_TYPE_KEYWORD_DETECTED = 0,
@@ -31,10 +58,11 @@ typedef enum {
 } xraudio_mfv_msg_type_t;
 
 typedef enum {
-   XRAUDIO_MFV_INPUT_ERROR_READ     = 0,
-   XRAUDIO_MFV_INPUT_ERROR_INTERNAL = 1,
-   XRAUDIO_MFV_INPUT_ERROR_INVALID  = 2
-} xraudio_mfv_input_error_t;
+   XRAUDIO_MFV_ERROR_AUDIO_REFERENCE_READ = 0,
+   XRAUDIO_MFV_ERROR_AUDIO_OUTPUT_WRITE   = 1,
+   XRAUDIO_MFV_ERROR_INTERNAL             = 2,
+   XRAUDIO_MFV_ERROR_INVALID              = 3
+} xraudio_mfv_error_t;
 
 typedef struct {
    xraudio_mfv_msg_type_t  type;
@@ -47,13 +75,35 @@ typedef struct {
 
 typedef struct {
    xraudio_mfv_msg_header_t  header;
-   xraudio_mfv_input_error_t type;
-} xraudio_mfv_msg_input_error_t;
+   xraudio_mfv_error_t       type;
+} xraudio_mfv_msg_error_t;
+
+typedef struct {
+   bool apply_gain;           ///< true if gain should be applied to the input audio stream, false otherwise
+   bool validate_keyword;     ///< true if keyword should be validated, false otherwise
+   bool detect_end_of_speech; ///< true if end of speech should be detected, false otherwise
+} xraudio_mfv_session_info_t;
+
+typedef struct {
+   uint8_t detection_type; ///< detection type (0 = detection while active, 1 = detection while asleep)
+   int16_t keyword_start;  ///< sample offset to start of keyword in audio stream (negative value indicates keyword start is unknown)
+   int16_t keyword_end;    ///< sample offset to end of keyword in audio stream (negative value indicates keyword end is unknown)
+   int16_t confidence;     ///< keyword detection confidence
+} xraudio_mfv_keyword_info_t;
 
 typedef struct {
    bool is_keyword_invalid; ///< true if the keyword has been declared invalid and should be ignored, false otherwise
    bool is_end_of_speech;   ///< true if the end of speech has been declared in the input audio stream, false otherwise
 } xraudio_mfv_process_result_t;
+
+typedef struct {
+   uint32_t                 total_audio_samples;    ///< total number of audio samples processed in the session
+   bool                     keyword_validated;      ///< true if the keyword has been validated, false otherwise
+   float                    keyword_confidence;     ///< the confidence of the keyword detection/validation (value between 0.0 and 1.0, or a negative value if not applicable)
+   xraudio_mfv_eos_result_t end_of_speech_result;   ///< result code for end of speech detection
+   bool                     gain_applied;           ///< true if gain has been applied to the input audio stream, false otherwise
+   float                    gain_value;             ///< the gain applied to the input audio stream in dB
+} xraudio_mfv_session_stats_t;
 
 typedef void *xraudio_mfv_object_t;
 
@@ -67,25 +117,29 @@ typedef xraudio_mfv_object_t (*xraudio_mfv_func_object_create_t)(const json_t *c
 
 typedef void                 (*xraudio_mfv_func_object_destroy_t)(xraudio_mfv_object_t object);
 
-typedef bool                 (*xraudio_mfv_func_session_open_t)(xraudio_mfv_object_t object, int output_fd);
+typedef xraudio_mfv_result_t (*xraudio_mfv_func_session_open_t)(xraudio_mfv_object_t object, xraudio_mfv_session_info_t *info, int *output_fd, xraudio_mfv_msg_callback_t callback);
 
-typedef void                 (*xraudio_mfv_func_session_close_t)(xraudio_mfv_object_t object);
+typedef void                 (*xraudio_mfv_func_session_close_t)(xraudio_mfv_object_t object, xraudio_mfv_session_stats_t *stats);
 
-typedef bool                 (*xraudio_mfv_func_session_process_audio_t)(xraudio_mfv_object_t object, const int16_t *sample_buffer, uint32_t sample_qty, xraudio_mfv_process_result_t *result);
+typedef xraudio_mfv_result_t (*xraudio_mfv_func_session_info_t)(xraudio_mfv_object_t object, xraudio_mfv_keyword_info_t *info);
 
-typedef bool                 (*xraudio_mfv_func_output_audio_open_t)(xraudio_mfv_object_t object, int input_fd, xraudio_mfv_msg_callback_t callback);
+typedef xraudio_mfv_result_t (*xraudio_mfv_func_session_process_audio_t)(xraudio_mfv_object_t object, const int16_t *sample_buffer, uint32_t sample_qty, xraudio_mfv_process_result_t *result);
 
-typedef void                 (*xraudio_mfv_func_output_audio_close_t)(xraudio_mfv_object_t object);
+typedef xraudio_mfv_result_t (*xraudio_mfv_func_reference_audio_open_t)(xraudio_mfv_object_t object, int fd, xraudio_mfv_msg_callback_t callback);
+
+typedef void                 (*xraudio_mfv_func_reference_audio_close_t)(xraudio_mfv_object_t object);
 
 typedef struct {
-   uint32_t                                       api_version;
+   uint16_t                                       api_version;
+   uint16_t                                       capabilities;
    xraudio_mfv_func_object_create_t               object_create;
    xraudio_mfv_func_object_destroy_t              object_destroy;
    xraudio_mfv_func_session_open_t                session_open;
    xraudio_mfv_func_session_close_t               session_close;
+   xraudio_mfv_func_session_info_t                session_info;
    xraudio_mfv_func_session_process_audio_t       session_process_audio;
-   xraudio_mfv_func_output_audio_open_t           output_audio_open;
-   xraudio_mfv_func_output_audio_close_t          output_audio_close;
+   xraudio_mfv_func_reference_audio_open_t        reference_audio_open;
+   xraudio_mfv_func_reference_audio_close_t       reference_audio_close;
 } xraudio_mfv_plugin_api_t;
 
 typedef xraudio_mfv_plugin_api_t *(*xraudio_mfv_plugin_api_get_t)(void);
