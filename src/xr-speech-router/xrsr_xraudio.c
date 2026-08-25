bc8fedb34b42bafa5722da5819f437ebfb156454
jthomp007c <jason_thomson@cable.comcast.com>
2026-08-25 09:27:22 -0400

    Update xrsr_xraudio.c
    Undoing the change to address a copilot comment because it complained about the change.
    We need to set stream->detecting to false here regardless of whether xraudio_detect_stop() failed or not.

--- src/xr-speech-router/xrsr_xraudio.c
+++ src/xr-speech-router/xrsr_xraudio.c
@@ -471,6 +471,7 @@ static void xrsr_xraudio_keyword_detect_stop(xrsr_xraudio_obj_t *obj) {
    if(!stream->detecting) { // already stopped
       return;
    }
+   stream->detecting = false;
 
    xraudio_result_t result = xraudio_detect_stop(obj->xraudio_obj);
 
@@ -479,7 +480,6 @@ static void xrsr_xraudio_keyword_detect_stop(xrsr_xraudio_obj_t *obj) {
        return;
    }
    obj->xraudio_state = XRSR_XRAUDIO_STATE_OPENED;
-   stream->detecting = false;
   
 }
 
