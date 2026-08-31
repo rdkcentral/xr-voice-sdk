19d1e3d3ad4f4f4750c6bdae37757773ea007f30
Michael Smith <michael_smith2@comcast.com>
2026-08-31 11:52:58 +0000

    Adding logging for on device testing

--- src/xr-speech-router/xrsr.c
+++ src/xr-speech-router/xrsr.c
@@ -2066,6 +2066,7 @@ void xrsr_msg_session_begin(const xrsr_thread_params_t *params, xrsr_thread_stat
                safec_rc = strncpy_s(http->transcription_in, sizeof(http->transcription_in), transcription_in, sizeof(http->transcription_in));
                ERR_CHK(safec_rc);
                http->transcription_in[sizeof(http->transcription_in) - 1] = '\0';
+               XLOGD_INFO("DIAG_STRNCPY_S http->transcription_in rc <%d> in_len <%zu> out_len <%zu> value <%s>", safec_rc, strlen(transcription_in), strlen(http->transcription_in), http->transcription_in);
                http->transcription_ptr = &http->transcription_in[0];
             }
 
@@ -2074,6 +2075,7 @@ void xrsr_msg_session_begin(const xrsr_thread_params_t *params, xrsr_thread_stat
                safec_rc = strncpy_s(http->audio_file_in, sizeof(http->audio_file_in), audio_file_in, sizeof(http->audio_file_in));
                ERR_CHK(safec_rc);
                http->audio_file_in[sizeof(http->audio_file_in) - 1] = '\0';
+               XLOGD_INFO("DIAG_STRNCPY_S http->audio_file_in rc <%d> in_len <%zu> out_len <%zu> value <%s>", safec_rc, strlen(audio_file_in), strlen(http->audio_file_in), http->audio_file_in);
             }
 
             // Call session begin handler
@@ -2133,6 +2135,7 @@ void xrsr_msg_session_begin(const xrsr_thread_params_t *params, xrsr_thread_stat
                   safec_rc = strncpy_s(ws->audio_file_in, sizeof(ws->audio_file_in), audio_file_in, sizeof(ws->audio_file_in));
                   ERR_CHK(safec_rc);
                   ws->audio_file_in[sizeof(ws->audio_file_in) - 1] = '\0';
+                  XLOGD_INFO("DIAG_STRNCPY_S ws->audio_file_in rc <%d> in_len <%zu> out_len <%zu> value <%s>", safec_rc, strlen(audio_file_in), strlen(ws->audio_file_in), ws->audio_file_in);
                }
 
                if(!begin->retry && ws->handlers.session_begin != NULL) { // Call session begin handler
